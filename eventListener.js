afdddd01c75dd491689e29a91adc9b3cbb676846
pukun1115-tech <pukun1115@gmail.com>
2026-07-04 13:07:33 +0900

    タッチ操作

--- eventListener.js
+++ eventListener.js
@@ -1,14 +1,32 @@
+let startX = 0;
+let startY = 0;
+let moved = false;
+
 window.addEventListener("resize", resize);
+
+canvas.addEventListener("pointerdown", (e) => {
+    e.preventDefault();
+    moved = false;
+    startX = e.clientX;
+    startY = e.clientY;
+})
 
 canvas.addEventListener("pointermove", (e) => {
     e.preventDefault();
     tileHighlight(e);
+
+    //動いたらドラッグ
+    if (Math.abs(e.clientX - startX) > 15 || Math.abs(e.clientY - startY) > 15) {
+        moved = true;
+    }
 });
 
 canvas.addEventListener("pointerup", (e) => {
     e.preventDefault();
-    tileHighlight(e);
 
+    if (moved) return;//動いたならタップではない
+
+    tileHighlight(e);
     if (!highlightTile) return;
     map[highlightTile.y][highlightTile.x] = 3;
 });
