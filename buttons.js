1bdaa6870ab51164eb456ba91f43457e2121a5f6
pukun1115-tech <pukun1115@gmail.com>
2026-07-06 08:53:34 +0900

    なたはたらやあや

--- buttons.js
+++ buttons.js
@@ -34,6 +34,10 @@ function drawSaveButton() {
     ctx.fillText("SAVE", tileSize * 11, (tate + 3) * tileSize);
 }
 
+function drawResetButton() {
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect( tileSize * 15, (tate + 2.5) * tileSize, tile * 2, tileSize);
+
 function onMoneyButtonClick() { }
 function onKabeButtonClick() { }
 function onTowerButtonClick() { }
