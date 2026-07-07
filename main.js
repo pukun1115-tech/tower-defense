2aa90043d10722496cebca76096b1ed6a93f44c3
pukun1115-tech <pukun1115@gmail.com>
2026-07-07 11:53:07 +0900

    Jfgiig

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-function drawMap() {
+function drawMap(c) {
     for (let y = 0; y < tate; y++) {
         for (let x = 0; x < yoko; x++) {
             switch (map[y][x]) {
                 case 0:
-                    ctx.fillStyle = "#008800";
+                    c.fillStyle = "#008800";
                     break;
                 case 1:
-                    ctx.fillStyle = "#ffffff";
+                    c.fillStyle = "#ffffff";
                     break;
                 case 2:
-                    ctx.fillStyle = "#aa4400";
+                    c.fillStyle = "#aa4400";
                     break;
                 case 3:
-                    ctx.fillStyle = "#000000";
+                    c.fillStyle = "#000000";
                     break;
             }
 
-            ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+            c.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
 
             if (tower[y][x] != 0) {
-                ctx.fillStyle = "#ffff00";
+                c.fillStyle = "#ffff00";
                 drawCircle(x * tileSize + tileSize / 2, y * tileSize + tileSize / 2, (tileSize / 2) * 0.8, ctx);
             }
         }
@@ -75,7 +75,7 @@ function loop() {
     time++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawMap();
+    drawMap(ctx);
     drawGrid();
     drawHighLight();
 
