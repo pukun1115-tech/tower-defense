b1446257474811e80a30920092c1ea880ae5a29a
pukun1115-tech <pukun1115@gmail.com>
2026-07-08 11:16:33 +0900

    Js

--- tower.js
+++ tower.js
@@ -1,1 +1,12 @@
-class tower
+class tower {
+    constructor(tileX, tileY) {
+        this.x = tileX + 0.5;
+        this.y = tileY + 0.5;
+    }
+    update() {
+        //
+    }
+    draw() {
+        drawCircle(this.x, this.y, 0.4, "#ffff00");
+    }
+}
