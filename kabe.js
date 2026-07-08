e3fbd59264958ccb81321fbfc3b39206d4f2a4e1
pukun1115-tech <pukun1115@gmail.com>
2026-07-08 10:02:03 +0900

    Delete kabe.js

--- kabe.js
+++
@@ -1,9 +0,0 @@
-function highlightCheck() {
-    highlightTile = getTileFromXY(pointerX, pointerY);
-}
-
-function placeKabeCheck() {
-    if (!highlightTile) return;
-    if (map[highlightTile.y][highlightTile.x] !== 0) return;
-    map[highlightTile.y][highlightTile.x] = 3;
-}
