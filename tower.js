9b7aa631096b864ca012a81328709f93d469c773
pukun1115-tech <pukun1115@gmail.com>
2026-07-10 07:47:51 +0900

    タワー

--- tower.js
+++ tower.js
@@ -1,9 +1,16 @@
 class tower {
-    constructor(tileX, tileY) {
+    constructor(tileX, tileY, attack, color, syurui, size) {
         this.x = tileX + 0.5;
         this.y = tileY + 0.5;
+        this.attack = attack;
+        this.color = color;
+        this.syurui = syurui;
+        this.size = size;
     }
     update() {
         //
+    }
+    draw() {
+        drawCircle(this.x, this.y, this.size, this.color);
     }
 }
