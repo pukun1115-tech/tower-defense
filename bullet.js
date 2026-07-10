c246c55638f8de391311a425d8801ad803d85a4a
pukun1115-tech <pukun1115@gmail.com>
2026-07-10 13:18:31 +0900

    まっすぐ

--- bullet.js
+++ bullet.js
@@ -2,7 +2,16 @@ class bullet{
   constructor(x, y, target, speed, size, color, damage){
     this.x = x;
     this.y = y;
+    
     this.target = target;
+    
+    const dx = this.target.x - this.x;
+    const dy = this.target.y - this.y;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+
+    this.dx = dx / dist;
+    this.dy = dy / dist;
+    
     this.speed = speed;
     this.size = size;
     this.color = color;
@@ -10,18 +19,26 @@ class bullet{
     this.alive = true;
   }
   update() {
-    const dx = this.target.x - this.x;
-    const dy = this.target.y - this.y;
-    const dist = Math.sqrt(dx * dx + dy * dy);
-    
-    if(dist < this.speed){
-      this.target.hp -= this.damage;
+    this.x += this.dx * this.speed;
+    this.y += this.dy * this.speed;
+
+    if(this.x < 0 || this.x > yoko || this.y < 0 || this.y > tate){
       this.alive = false;
       return;
     }
-    
-    this.x += (dx / dist) * this.speed;
-    this.y += (dy / dist) * this.speed;
+    for (const e of enemies) {
+      if(!e.alive) continue;
+
+      const dx = e.x - this.x;
+      const dy = e.y - this.y;
+      const dist = Math.sqrt(dx * dx + dy + dy);
+
+      if(dist < this.size + e.size){
+        e.hp -= this.damage;
+        this.alive = false;
+        break;
+      }
+    }
   }
   draw(){
     drawCircle(this.x, this.y, this.size, this.color);
