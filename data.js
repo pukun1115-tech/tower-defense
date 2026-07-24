a6d6b6349146f20b894549795685e36dc5cf944b
pukun1115-tech <pukun1115@gmail.com>
2026-07-24 21:16:00 +0900

    Update data.js

--- data.js
+++ data.js
@@ -66,28 +66,28 @@ let towers = [];
 let bullets = [];
 let enemies = [];
 const towerTypes = {
-    4: { damage: 1, color: "#ffff00", size: 0.3, range: 2, cooldown: 60, cost: 20, bulletSpeed: 0.1, bulletSize: 0.2, kan: true },
+    4: { damage: 1, color: "#ffff00", size: 0.3, range: 2, cooldown: 60, cost: 20, bulletSpeed: 0.1, bulletSize: 0.05, kan: true },
     5: { damage: 2, color: "#ff00ff", size: 0.4, range: 3, cooldown: 30, cost: 60, bulletSpeed: 0.2, bulletSize: 0.1, kan: false },
-    6: { damage: 5, color: "#5522ff", size: 0.35, range: 8, cooldown: 720, cost: 100, bulletSpeed: 0.3, bulletSize: 0.1, kan: true },
+    6: { damage: 5, color: "#5522ff", size: 0.35, range: 8, cooldown: 720, cost: 100, bulletSpeed: 0.3, bulletSize: 0.2, kan: true },
     7: { damage: 1, color: "#00ff00", size: 0.25, range: 3, cooldown: 10, cost: 150, bulletSpeed: 0.2, bulletSize: 0.1, kan: false }
 };
 const enemyTypes = {
     //基本
-    0: { hp: 5, speed: 0.02, color: "#0000ff", size: 0.3, money: 5 },
+    0: { hp: 5, speed: 0.02, color: "#0000ff", size: 0.3, money: 2 },
     //ちょっと強い
-    1: { hp: 15, speed: 0.015, color: "#ffcc00", size: 0.3, money: 10 },
+    1: { hp: 15, speed: 0.015, color: "#ffcc00", size: 0.3, money: 6 },
     //足が速い
     2: { hp: 2, speed: 0.05, color: "#00ffff", size: 0.2, money: 1 },
     //小ボス
-    3: { hp: 100, speed: 0.005, color: "#404040", size: 0.4, money: 50 },
+    3: { hp: 100, speed: 0.005, color: "#404040", size: 0.4, money: 20 },
     //中ボス
-    4: { hp: 600, speed: 0.01, color: "#000000", size: 0.5, money: 200 },
+    4: { hp: 600, speed: 0.01, color: "#000000", size: 0.5, money: 75 },
     //強い
-    5: { hp: 100, speed: 0.02, color: "#8000ff", size: 0.3, money: 5 },
+    5: { hp: 100, speed: 0.02, color: "#8000ff", size: 0.3, money: 1 },
     //めちゃ足速い
     6: { hp: 10, speed: 0.1, color: "#00ffff", size: 0.1, money: 3 },
     //死んだら大量の敵を出す
-    7: { hp: 100, speed: 0.01, color: "#000000", size: 0.1, money: 30 }
+    7: { hp: 100, speed: 0.01, color: "#000000", size: 0.1, money: 2 }
 };
 //その他
 const dirs = [
