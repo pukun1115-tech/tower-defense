070e69d7e990ed6bb70ce2f943d010dd7d1a996e
pukun1115-tech <pukun1115@gmail.com>
2026-07-31 14:00:13 +0900

    ついにダメージが綺麗に描けるはず

--- bullet.js
+++ bullet.js
@@ -36,7 +36,7 @@ class bullet {
             const dist = Math.sqrt(dx * dx + dy * dy);
 
             if (dist < this.size + e.size && !this.hairetu.has(e.id)) {
-                const dam = { damage: this.damage, x: e.x, y: e.y, time: Game.time, kosa: 60 };
+                const dam = { damage: this.damage, x: e.x, y: e.y, time: Game.time, kosa: 0.6 };
                 damages.push(dam);
                 this.hp--;
                 e.hp -= this.damage;
