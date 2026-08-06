be557244309139ddd555e49b6db6e942f3be1677
pukun1115-tech <pukun1115@gmail.com>
2026-08-06 14:40:41 +0900

    LevelUp改良開始

--- buttonDraw.js
+++ buttonDraw.js
@@ -16,30 +16,30 @@ function drawLevelUpButton() {
 }
 
 function drawMoneyLevelUpButton() {
-    drawShikakuRect(1, tate + 0.5, 8, 1.5, "#ffffff");
+    drawShikakuRect(1, tate + 2.5, 8, 1.5, "#ffffff");
     ctx.fillStyle = "#000000";
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.font = `${fontSize}px sans-serif`;
     if (Game.moneyLevel < 10) {
-        ctx.fillText("お金レベルアップ", 4 * tileSize, (tate + 1.25) * tileSize);
-        ctx.fillText(moneyLevelHyou[Game.moneyLevel + 1].cost + "$", 8 * tileSize, (tate + 1.25) * tileSize);
+        ctx.fillText("お金レベルアップ", 4 * tileSize, (tate + 3.25) * tileSize);
+        ctx.fillText(moneyLevelHyou[Game.moneyLevel + 1].cost + "$", 8 * tileSize, (tate + 3.25) * tileSize);
         if (Game.money < moneyLevelHyou[Game.moneyLevel + 1].cost) {
-            drawShikakuRect(1, tate + 0.5, 8, 1.5, "#00000080");
+            drawShikakuRect(1, tate + 2.5, 8, 1.5, "#00000080");
         }
     }
     else {
-        ctx.fillText("LevelMax!", 5 * tileSize, (tate + 1.25) * tileSize);
+        ctx.fillText("LevelMax!", 5 * tileSize, (tate + 3.25) * tileSize);
     }
 }
 
 function drawTowerLevelUpButton() {
-    drawShikakuRect(10, tate + 0.5, 7, 1.5, "#ffffff");
+    drawShikakuRect(10, tate + 2.5, 7, 1.5, "#ffffff");
     ctx.fillStyle = "#000000";
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.font = `${fontSize}px sans-serif`;
-    ctx.fillText("タワーレベルアップ", 13.5 * tileSize, (tate + 1.25) * tileSize);
+    ctx.fillText("タワーレベルアップ", 13.5 * tileSize, (tate + 3.25) * tileSize);
 }
 
 function drawKabeButton() {
